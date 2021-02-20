a5fd9c1c0ca89c41fb9caa4208c899a22a1c230c
beyda1022@hotmail.com <beyda1022@hotmail.com>
2021-02-21 01:06:36 +0300

    odev1

---
+++ Business/DependencyResolves/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
+using Castle.DynamicProxy;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolves.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CarManager>().As<ICarService>().SingleInstance();
+            builder.RegisterType<EfCarDal>().As<ICarDal>().SingleInstance();
+            
+        }
+    }
+}
